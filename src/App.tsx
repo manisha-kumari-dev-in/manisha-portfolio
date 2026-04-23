b7fa677f160f09a131c3103a3fd3b22ff4928ed5
manisha-kumari-dev-in <manisha.kumari.dev.in@gmail.com>
2026-04-23 23:12:26 +0530

    chore: initial project setup with React, Vite, Tailwind, and GSAP

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl text-blue-500 font-bold">
+        Manisha Portfolio 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
